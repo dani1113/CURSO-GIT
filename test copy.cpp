bf33f5146bf6c2cd7694fc3694639eeae83ffd6a
Daniel Carmona Salazar <danirm1113@gmail.com>
2023-05-09 17:39:36 +0200

    commit en rama 2, nuevo cambio en scd:out

--- test copy.cpp
+++ test copy.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Test nuevo nuevo jsadjsadjjasd NUEVO CAMBIO EN RAMA 1" << std::endl;
+    std::cout << "Test nuevo nuevo jsadjsadjjasd NUEVO CAMBIO EN RAMA 1 ------------ERROR ME ESTTOY EQUIVOCANDO-----" << std::endl;
     std::cout << "Nuevo cambio" << std::endl;
     return 0;
 }
